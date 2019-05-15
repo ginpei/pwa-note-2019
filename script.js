abdc236ffca52bff8356746d1c0e88f32968dd89
ginpei <ginpei@gmail.com>
2019-05-14 22:09:52 -0700

    extract preference dialog handler

--- script.js
+++ script.js
@@ -2,22 +2,29 @@ import Editor from './Editor.js';
 import * as Preferences from './Preferences.js';
 import PreferencesDialog from './PreferencesDialog.js';
 
+function openPreferenceDialog () {
+  return new Promise(async (resolve) => {
+    const preferences = await Preferences.load();
+    const dialog = new PreferencesDialog({
+      el: document.querySelector('#preferencesDialog'),
+      onComplete: (preferences) => {
+        Preferences.save(preferences);
+        resolve(preferences);
+      },
+      preferences,
+    });
+    dialog.open();
+  });
+}
+
 async function main () {
   const preferences = await Preferences.load();
 
   const editor = new Editor({
     el: document.querySelector('#editor'),
     async onPreferences () {
-      const currentPref = await Preferences.load();
-      const dialog = new PreferencesDialog({
-        el: document.querySelector('#preferencesDialog'),
-        onComplete: (preferences) => {
-          editor.setPreferences(preferences);
-          Preferences.save(preferences);
-        },
-        preferences: currentPref,
-      });
-      dialog.open();
+      const newPref = await openPreferenceDialog();
+      editor.setPreferences(newPref);
     },
     preferences,
   });
